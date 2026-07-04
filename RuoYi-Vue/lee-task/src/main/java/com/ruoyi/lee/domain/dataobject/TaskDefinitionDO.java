6d92186bc4062c2f8523437eac6ebee0c027c460
Leezed525 <leezed525@qq.com>
2026-07-04 23:39:25 +0800

    测试完成TaskDefinition和TaskInstance的数据库插入，删改查应该不用测了

--- RuoYi-Vue/lee-task/src/main/java/com/ruoyi/lee/domain/dataobject/TaskDefinitionDO.java
+++ RuoYi-Vue/lee-task/src/main/java/com/ruoyi/lee/domain/dataobject/TaskDefinitionDO.java
@@ -1,5 +1,8 @@
 package com.ruoyi.lee.domain.dataobject;
 
+import com.baomidou.mybatisplus.annotation.IdType;
+import com.baomidou.mybatisplus.annotation.TableId;
+import com.baomidou.mybatisplus.annotation.TableName;
 import java.time.LocalDateTime;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -8,8 +11,10 @@ import lombok.experimental.Accessors;
 @Data
 @NoArgsConstructor
 @Accessors(chain = true)
+@TableName("lee_task_definition")
 public class TaskDefinitionDO {
 
+	@TableId(value = "id", type = IdType.INPUT)
 	private String id;
 
 	private String name;
